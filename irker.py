7087eeac98f08e27b8b2b8ed7c92ed827d327744
Eric S. Raymond <esr@thyrsus.com>
2012-08-25 07:06:10 -0400

    Run loop is working.

--- irker.py
+++ irker.py
@@ -6,5 +6,30 @@ Takes JSON objects of the form {'channel':<channel-url>, 'message':<text>}
 and relays to IRC channels.
 
 """
-import os, sys, json, irclib
+import os, sys, json, irclib, getopt
 
+class Irker:
+    "Persistent IRC multiplexer."
+    def __init__(self):
+        self.botpool = {}
+    def logerr(self, errmsg):
+        "Log a processing error."
+        sys.stderr.write(errmsg)
+    def run(self, ifp):
+        "Accept JSON relay requests from specified stream."
+        while True:
+            inp = ifp.readline()
+            if not inp:
+                break
+            try:
+                request = json.loads(inp.strip())
+            except ValueError:
+                self.logerr("irker: can't recognize JSON on input.\n")
+                break
+            self.relay(request)
+    def relay(self, request):
+        print request
+
+if __name__ == '__main__':
+    irker = Irker()
+    irker.run(sys.stdin)
