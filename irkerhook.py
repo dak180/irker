dbcb19860e69df9966bc5bb3d1344116368db8af
Eric S. Raymond <esr@thyrsus.com>
2012-10-03 00:30:30 -0400

    Version bump for release.

--- irkerhook.py
+++ irkerhook.py
@@ -39,7 +39,7 @@ default_channels = "irc://chat.freenode.net/%(project)s,irc://chat.freenode.net/
 
 import os, sys, commands, socket, urllib, json
 
-version = "1.4"
+version = "1.5"
 
 def shellquote(s):
     return "'" + s.replace("'","'\\''") + "'"
