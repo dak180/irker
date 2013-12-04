c088f7f814abc9ada8e07fee2b494a6a4da09ad9
Eric S. Raymond <esr@thyrsus.com>
2013-12-03 19:35:21 -0500

    Version bump for release 2.4.

--- irkerhook.py
+++ irkerhook.py
@@ -37,7 +37,7 @@ default_channels = "irc://chat.freenode.net/#commits"
 # No user-serviceable parts below this line:
 #
 
-version = "2.2"
+version = "2.4"
 
 import os, sys, commands, socket, urllib, subprocess, locale, datetime
 from pipes import quote as shellquote
